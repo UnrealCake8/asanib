61d7748234de8f00242922236ff5e1aea8c2d7be
H A <unrealcake8andfriends@proton.me>
2026-09-11 21:18:59 +0400

    Load customer account styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { registerSW } from 'virtual:pwa-register'
 import App from './App'
 import './styles.css'
 import './functional.css'
+import './customer.css'
 
 registerSW({ immediate: true })
 
