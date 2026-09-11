fd2b2403ee275fc87740269a4442fdaa53be3e93
H A <unrealcake8andfriends@proton.me>
2026-09-11 21:36:35 +0400

    Resync existing push registrations on app start

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
 import App from './App'
+import { watchAuth } from './lib/data'
+import { syncPushIfAlreadyAllowed } from './lib/notifications'
 import './styles.css'
 import './functional.css'
 import './customer.css'
 
 registerSW({ immediate: true })
+
+watchAuth((user) => {
+  if (user) void syncPushIfAlreadyAllowed()
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
